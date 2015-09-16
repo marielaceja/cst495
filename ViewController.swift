4777e0b6fdf34a5fcfdd661b23ebe4cf7bbcbe94
mariela <maceja@csumb.edu>
2015-09-15 22:43:33 -0700

    Calc_Assignment1
    This is the initial code for the Calculator.

---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mariela Ceja on 9/14/15.
+//  Copyright (c) 2015 Mariela Ceja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit
+        }
+        else{
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
+        
+    }
+    
+
+}
+
